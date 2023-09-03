a54a9fab232844a2aab9808086034c01829280a8
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-09-03 18:29:02 +0300

    Add project files.

---
+++ WPFCode.Lab1/Gradient.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WPFCode.Lab1;
+
+public partial class Gradient : Window
+{
+    public Gradient()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WPFCode.Lab1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFCode.Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //await ShowWindowWithDelay(new Gradient());
+        }
+
+        private async Task ShowWindowWithDelay(Window window, int? delay = null)
+        {
+            if (delay.HasValue)
+            {
+                await Task.Delay(delay.Value);
+            }
+
+            window.Show();
+        }
+
+        private async void OpenNewWindow_Click(object sender, RoutedEventArgs e)
+        {
+            await ShowWindowWithDelay(new Gradient());
+        }
+    }
+}
