dcaf43168a5ebb1a253cbdd8d8b464f622ee980f
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 14:38:27 +0300

    add lab7 variant 7

---
+++ Lab7.Variant07.Library/CharacterCountTransformer.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+
+namespace Lab7.Variant07.Library;
+
+public class CharacterCountTransformer
+{
+    public string TransformRepeatingCharacters(string text)
+    {
+        // Разделяем строки на подстроки по следующим символам [' ', '\t', '\n']
+        // StringSplitOptions.RemoveEmptyEntries - удаляеn пустые элементы нового массива
+        var words = text.Split(new char[] {' ', '\t', '\n'},
+            StringSplitOptions.RemoveEmptyEntries);
+        
+        var transformedWords = new List<string>();
+
+        // Проходим по всем элементам массива и добавляем их в массив преобразованных строк
+        foreach (var word in words)
+        {
+            transformedWords.Add(TransformWord(word));
+        }
+
+        // Объединяем все подстроки
+        return string.Join(" ", transformedWords);
+    }
+
+    private string TransformWord(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return word;
+        }
+
+        StringBuilder transformedWord = new StringBuilder();
+        int count = 1;
+        char lastChar = word[0];
+
+        // Проходим по всем символам строки
+        for (int i = 1; i < word.Length; i++)
+        {
+            if (word[i] == lastChar)
+            {
+                count++;
+            }
+            else
+            {
+                AppendTransformedCharacter(transformedWord, lastChar, count);
+                lastChar = word[i];
+                count = 1;
+            }
+        }
+        
+        AppendTransformedCharacter(transformedWord, lastChar, count);
+
+        return transformedWord.ToString();
+    }
+
+    private void AppendTransformedCharacter(StringBuilder sb, char character, int count)
+    {
+        sb.Append(character);
+        if (count > 1)
+        {
+            sb.Append(count);
+        }
+    }
+}
---
+++ Lab7.Variant07.Tests/CharacterCountTransformerTests.cs
@@ -0,0 +1,51 @@
+﻿using Lab7.Variant07.Library;
+using Xunit;
+
+namespace Lab7.Variant07.Tests;
+
+public class CharacterCountTransformerTests
+{
+    private readonly CharacterCountTransformer _characterCountTransformer = new CharacterCountTransformer();
+
+    [Fact]
+    public void TransformRepeatingCharacters_StandardCase_TransformsCorrectly()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("aaabb ccccddd eeefggg hhkl");
+        Assert.Equal("a3b2 c4d3 e3fg3 h2kl", result);
+    }
+
+    [Fact]
+    public void TransformRepeatingCharacters_MixedCase_RemainsCaseSensitive()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("Coffeeee");
+        Assert.Equal("Cof2e4", result);
+    }
+
+    [Fact]
+    public void TransformRepeatingCharacters_WithPunctuation_HandlesCorrectly()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("Hello, world! WWooorld!!!");
+        Assert.Equal("Hel2o, world! W2o3rld!3", result);
+    }
+
+    [Fact]
+    public void TransformRepeatingCharacters_EmptyString_ReturnsEmpty()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void TransformRepeatingCharacters_NoDuplicates_NoChange()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("Unique");
+        Assert.Equal("Unique", result);
+    }
+
+    [Fact]
+    public void TransformRepeatingCharacters_SingleCharacterStrings_NoChange()
+    {
+        string result = _characterCountTransformer.TransformRepeatingCharacters("a b c d e");
+        Assert.Equal("a b c d e", result);
+    }
+}
