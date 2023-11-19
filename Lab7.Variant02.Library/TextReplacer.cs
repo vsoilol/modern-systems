f8c32bfdf3cbea0507dfa2bdc27654f5a9ed0106
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 10:24:59 +0300

    add lab7 variant 2

---
+++ Lab7.Variant02.Library/TextReplacer.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab7.Variant02.Library;
+
+public class TextReplacer
+{
+    public string ReplaceLetterInText(string text, char oldLetter, string newCombination)
+    {
+        // Разделяем строки на подстроки по следующим символам [' ', '\t', '\n']
+        // StringSplitOptions.RemoveEmptyEntries - удаляеn пустые элементы нового массива
+        var words = text.Split(new [] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        // Проходим по всем элементам массива и заменяем все значения oldLetter на newCombination
+        for (var i = 0; i < words.Length; i++)
+        {
+            words[i] = words[i].Replace(oldLetter.ToString(), newCombination);
+        }
+
+        // Объединяем все подстроки
+        return string.Join(" ", words);
+    }
+}
---
+++ Lab7.Variant02.Tests/TextReplacementTests.cs
@@ -0,0 +1,51 @@
+﻿using Lab7.Variant02.Library;
+using Xunit;
+
+namespace Lab7.Variant02.Tests;
+
+public class TextReplacerTests
+{
+    private readonly TextReplacer _textReplacer = new TextReplacer();
+
+    [Fact]
+    public void ReplaceLetterInText_SingleOccurrence_ReplacesCorrectly()
+    {
+        var result = _textReplacer.ReplaceLetterInText("test", 't', "tt");
+        Assert.Equal("ttestt", result);
+    }
+
+    [Fact]
+    public void ReplaceLetterInText_MultipleOccurrences_ReplacesAll()
+    {
+        var result = _textReplacer.ReplaceLetterInText("test text", 't', "tt");
+        Assert.Equal("ttestt ttextt", result);
+    }
+
+    [Fact]
+    public void ReplaceLetterInText_NoOccurrences_NoChange()
+    {
+        var result = _textReplacer.ReplaceLetterInText("hello world", 'x', "XX");
+        Assert.Equal("hello world", result);
+    }
+
+    [Fact]
+    public void ReplaceLetterInText_EmptyString_ReturnsEmpty()
+    {
+        var result = _textReplacer.ReplaceLetterInText("", 'a', "b");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void ReplaceLetterInText_SpecialCharacters_ReplacesCorrectly()
+    {
+        var result = _textReplacer.ReplaceLetterInText("hello! world?", 'o', "OO");
+        Assert.Equal("hellOO! wOOrld?", result);
+    }
+
+    [Fact]
+    public void ReplaceLetterInText_CaseSensitive_ReplacesOnlyMatchingCase()
+    {
+        var result = _textReplacer.ReplaceLetterInText("Hello World", 'o', "OO");
+        Assert.Equal("HellOO WOOrld", result);
+    }
+}
