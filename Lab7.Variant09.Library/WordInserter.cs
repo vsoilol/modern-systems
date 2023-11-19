be50b99d46229a83c3ed9c7e18351b5fcfe3628c
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 15:03:37 +0300

    add lab7 variant 9

---
+++ Lab7.Variant09.Library/WordInserter.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Lab7.Variant09.Library;
+
+public class WordInserter
+{
+    public string InsertWordInText(string text, string wordToFind, string wordToInsert)
+    {
+        StringBuilder transformedText = new StringBuilder();
+        string[] words = text.Split(new[] {' ', '\t', '\n'},
+            StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            transformedText.Append(words[i]);
+            if (words[i].Equals(wordToFind, StringComparison.OrdinalIgnoreCase))
+            {
+                transformedText.Append(" " + wordToInsert);
+            }
+
+            if (i < words.Length - 1)
+            {
+                transformedText.Append(" ");
+            }
+        }
+
+        return transformedText.ToString();
+    }
+}
---
+++ Lab7.Variant09.Tests/WordInserterTests.cs
@@ -0,0 +1,44 @@
+﻿using Lab7.Variant09.Library;
+using Xunit;
+
+namespace Lab7.Variant09.Tests;
+
+public class WordInserterTests
+{
+    private readonly WordInserter _wordInserter = new WordInserter();
+
+    [Fact]
+    public void InsertWordInText_SingleOccurrence_InsertsCorrectly()
+    {
+        string result = _wordInserter.InsertWordInText("The quick brown fox", "fox", "jumps");
+        Assert.Equal("The quick brown fox jumps", result);
+    }
+
+    [Fact]
+    public void InsertWordInText_MultipleOccurrences_InsertsInAll()
+    {
+        string result = _wordInserter.InsertWordInText("Hello world world hello", "world", "wide");
+        Assert.Equal("Hello world wide world wide hello", result);
+    }
+
+    [Fact]
+    public void InsertWordInText_NoMatchingWord_NoChange()
+    {
+        string result = _wordInserter.InsertWordInText("Hello world", "planet", "earth");
+        Assert.Equal("Hello world", result);
+    }
+
+    [Fact]
+    public void InsertWordInText_EmptyString_ReturnsEmpty()
+    {
+        string result = _wordInserter.InsertWordInText("", "hello", "hi");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void InsertWordInText_CaseInsensitive_MatchesAndInserts()
+    {
+        string result = _wordInserter.InsertWordInText("Hello World", "world", "wide");
+        Assert.Equal("Hello World wide", result);
+    }
+}
