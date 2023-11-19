3fe30b59303599b5fe0a08729960d222b6940f00
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 13:49:39 +0300

    add lab7 variant 3

---
+++ Lab7.Variant03.Library/LetterRemover.cs
@@ -0,0 +1,42 @@
+﻿namespace Lab7.Variant03.Library;
+
+public class LetterRemover
+{
+    public string RemoveLetterBetween(string text, char firstLetter, char secondLetter)
+    {
+        // Разделяем строки на подстроки по следующим символам [' ', '\t', '\n']
+        // StringSplitOptions.RemoveEmptyEntries - удаляеn пустые элементы нового массива
+        var words = text.Split(new char[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+        
+        // Проходим по всем элементам массива
+        for (int i = 0; i < words.Length; i++)
+        {
+            words[i] = RemoveLetterInWord(words[i], firstLetter, secondLetter);
+        }
+
+        // Объединяем все подстроки
+        return string.Join(" ", words);
+    }
+
+    private string RemoveLetterInWord(string word, char firstChar, char secondChar)
+    {
+        // Находим индекс первого вхождения символа firstChar
+        int firstIndex = word.IndexOf(firstChar);
+        
+        // Пока мы не дойдём до конца массива
+        while (firstIndex != -1 && firstIndex + 2 < word.Length)
+        {
+            // Если символ который находиться на две поизиции больше чем firstChar равняется secondChar
+            // Тогда удаляем символ находящийся между первым вхождением firstChar и secondChar
+            if (word[firstIndex + 2] == secondChar)
+            {
+                word = word.Remove(firstIndex + 1, 1);
+            }
+
+            // Находим новое вхождение firstChar в строке word начиная с индекса firstIndex + 1
+            firstIndex = word.IndexOf(firstChar, firstIndex + 1);
+        }
+        
+        return word;
+    }
+}
---
+++ Lab7.Variant03.Tests/CharacterRemoverTests.cs
@@ -0,0 +1,58 @@
+﻿using Lab7.Variant03.Library;
+using Xunit;
+
+namespace Lab7.Variant03.Tests;
+
+public class CharacterRemoverTests
+{
+    private readonly LetterRemover _letterRemover = new LetterRemover();
+
+    [Fact]
+    public void RemoveLetterBetween_StandardCase_RemovesCorrectCharacter()
+    {
+        string result = _letterRemover.RemoveLetterBetween("example axa bxb", 'a', 'a');
+        Assert.Equal("example aa bxb", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_NoOccurrences_NoChange()
+    {
+        string result = _letterRemover.RemoveLetterBetween("test text", 'x', 'z');
+        Assert.Equal("test text", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_EmptyString_ReturnsEmpty()
+    {
+        string result = _letterRemover.RemoveLetterBetween("", 'a', 'b');
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_SpecialCharacters_RemovesCorrectCharacter()
+    {
+        string result = _letterRemover.RemoveLetterBetween("he!l?lo w?orld", '!', '?');
+        Assert.Equal("he!?lo w?orld", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_CaseSensitive_RemovesOnlyMatchingCase()
+    {
+        string result = _letterRemover.RemoveLetterBetween("Hello World", 'H', 'l');
+        Assert.Equal("Hllo World", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_MultipleOccurrences_RemovesAllCorrectCharacters()
+    {
+        string result = _letterRemover.RemoveLetterBetween("abaracadabra", 'a', 'a');
+        Assert.Equal("aaaaabra", result);
+    }
+
+    [Fact]
+    public void RemoveLetterBetween_AdjacentPatterns_HandlesCorrectly()
+    {
+        string result = _letterRemover.RemoveLetterBetween("aaxaa", 'a', 'a');
+        Assert.Equal("aaaa", result);
+    }
+}
