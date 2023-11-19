cdadf500d316231312515ab6f6cff9d2017a5996
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 15:14:15 +0300

    add lab7 variant 10

---
+++ Lab7.Variant10.Library/SentenceVowelSorter.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab7.Variant10.Library;
+
+public class SentenceVowelSorter
+{
+    public string SortSentencesByVowelCount(string text)
+    {
+        char[] vowelChars = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        char[] wordDelimiters = {' ', '\t', '\n'};
+
+        var words = text.Split(wordDelimiters, StringSplitOptions.RemoveEmptyEntries);
+        var sortedSentences = new List<string>();
+        
+        Array.Sort(words, (w1, w2) => CountVowels(w1, vowelChars).CompareTo(CountVowels(w2, vowelChars)));
+
+        sortedSentences.Add(string.Join(" ", words));
+
+        return string.Join(" ", sortedSentences);
+    }
+
+    private int CountVowels(string word, char[] vowels)
+    {
+        int vowelCount = 0;
+        foreach (char c in word)
+        {
+            if (Array.IndexOf(vowels, c) >= 0)
+            {
+                vowelCount++;
+            }
+        }
+
+        return vowelCount;
+    }
+}
---
+++ Lab7.Variant10.Tests/SentenceVowelSorterTests.cs
@@ -0,0 +1,51 @@
+﻿using Lab7.Variant10.Library;
+using Xunit;
+
+namespace Lab7.Variant10.Tests;
+
+public class SentenceVowelSorterTests
+{
+    private readonly SentenceVowelSorter _sentenceVowelSorter = new SentenceVowelSorter();
+
+    [Fact]
+    public void SortSentencesByVowelCount_StandardCase_SortsCorrectly()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("This is a test. Vowels are important.");
+        Assert.Equal("This is a test. Vowels are important.", result);
+    }
+
+    [Fact]
+    public void SortSentencesByVowelCount_SingleSentence_SortsCorrectly()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("Sorting words in a sentence.");
+        Assert.Equal("words in a Sorting sentence.", result);
+    }
+
+    [Fact]
+    public void SortSentencesByVowelCount_EmptyText_ReturnsEmpty()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void SortSentencesByVowelCount_EqualVowelsCount_SortsCorrectly()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("Gym is fun.");
+        Assert.Equal("Gym is fun.", result);
+    }
+
+    [Fact]
+    public void SortSentencesByVowelCount_AllVowels_SortsCorrectly()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("I like to eat apples and bananas.");
+        Assert.Equal("I to and like eat apples bananas.", result);
+    }
+
+    [Fact]
+    public void SortSentencesByVowelCount_PunctuationHandling_SortsCorrectly()
+    {
+        string result = _sentenceVowelSorter.SortSentencesByVowelCount("Hello, world! What a beautiful day.");
+        Assert.Equal("world! What a day. Hello, beautiful", result);
+    }
+}
