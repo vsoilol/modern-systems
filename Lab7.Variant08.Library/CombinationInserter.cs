a56a2897e445a200c8665fe5ff3aa54fe8c7c474
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 14:55:47 +0300

    add lab7 variant 8

---
+++ Lab7.Variant08.Library/CombinationInserter.cs
@@ -0,0 +1,41 @@
+﻿namespace Lab7.Variant08.Library;
+
+public class CombinationInserter
+{
+    public string InsertCombinationInText(string text, string threeLetterCombo, string twoLetterCombo)
+    {
+        // Разделяем строки на подстроки по следующим символам [' ', '\t', '\n']
+        // StringSplitOptions.RemoveEmptyEntries - удаляеn пустые элементы нового массива
+        var words = text.Split(new[] {' ', '\t', '\n'},
+            StringSplitOptions.RemoveEmptyEntries);
+        
+        var transformedWords = new List<string>();
+
+        // Проходим по всем элементам массива и добавляем их в массив преобразованных строк
+        foreach (var word in words)
+        {
+            transformedWords.Add(InsertCombinationInWord(word, threeLetterCombo, twoLetterCombo));
+        }
+
+        // Объединяем все подстроки
+        return string.Join(" ", transformedWords);
+    }
+
+    private string InsertCombinationInWord(string word, string threeLetterCombo, string twoLetterCombo)
+    {
+        // Находим индекс первого вхождения threeLetterCombo в строке word
+        int index = word.IndexOf(threeLetterCombo);
+        
+        // Проходимся по всей строке и добавляем после всех threeLetterCombo twoLetterCombo
+        while (index != -1)
+        {
+            // вставляем twoLetterCombo после threeLetterCombo
+            word = word.Insert(index + threeLetterCombo.Length, twoLetterCombo);
+
+            // Находим индекс следующего вхождения threeLetterCombo
+            index = word.IndexOf(threeLetterCombo, index + threeLetterCombo.Length + twoLetterCombo.Length);
+        }
+
+        return word;
+    }
+}
---
+++ Lab7.Variant08.Tests/CombinationInserterTests.cs
@@ -0,0 +1,51 @@
+﻿using Lab7.Variant08.Library;
+using Xunit;
+
+namespace Lab7.Variant08.Tests;
+
+public class CombinationInserterTests
+{
+    private readonly CombinationInserter _combinationInserter = new CombinationInserter();
+
+    [Fact]
+    public void InsertCombinationInText_StandardCase_InsertsCorrectly()
+    {
+        string result = _combinationInserter.InsertCombinationInText("The quick brown fox", "qui", "ck");
+        Assert.Equal("The quickck brown fox", result);
+    }
+
+    [Fact]
+    public void InsertCombinationInText_MultipleOccurrences_InsertsInAll()
+    {
+        string result = _combinationInserter.InsertCombinationInText("Abracadabra abra cadabra", "abr", "ka");
+        Assert.Equal("Abracadabrkaa abrkaa cadabrkaa", result);
+    }
+
+    [Fact]
+    public void InsertCombinationInText_NoMatchingCombination_NoChange()
+    {
+        string result = _combinationInserter.InsertCombinationInText("Hello world", "xyz", "ab");
+        Assert.Equal("Hello world", result);
+    }
+
+    [Fact]
+    public void InsertCombinationInText_EmptyString_ReturnsEmpty()
+    {
+        string result = _combinationInserter.InsertCombinationInText("", "any", "th");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void InsertCombinationInText_CaseSensitive_DoesNotInsert()
+    {
+        string result = _combinationInserter.InsertCombinationInText("Sensitive Case", "cas", "XX");
+        Assert.Equal("Sensitive Case", result);
+    }
+
+    [Fact]
+    public void InsertCombinationInText_WithPunctuation_HandlesCorrectly()
+    {
+        string result = _combinationInserter.InsertCombinationInText("Hello, world! Wonderful world.", "wor", "ly");
+        Assert.Equal("Hello, worlyld! Wonderful worlyld.", result);
+    }
+}
