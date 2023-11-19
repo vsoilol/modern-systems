937da37e78a44eb48e447a856dee4f67d51a50cd
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 14:26:21 +0300

    add lab7 variant 6

---
+++ Lab7.Variant06.Library/RepeatedLetterRemover.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+
+namespace Lab7.Variant06.Library;
+
+public class RepeatedLetterRemover
+{
+    public string RemoveRepeatingCharacters(string text)
+    {
+        // Разделяем строки на подстроки по следующим символам [' ', '\t', '\n']
+        // StringSplitOptions.RemoveEmptyEntries - удаляеn пустые элементы нового массива
+        var words = text.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+       
+        // Проходим по всем элементам массива
+        for (var i = 0; i < words.Length; i++)
+        {
+            words[i] = RemoveRepeatingCharactersFromWord(words[i]);
+        }
+
+        // Объединяем все подстроки
+        return string.Join(" ", words);
+    }
+
+    private string RemoveRepeatingCharactersFromWord(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return word;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        
+        // '\0' - Null character, представляет собой конец строки
+        char lastChar = '\0';
+        
+        // Проходим по всем символам строки и создаём новую строку без дублирующихся символов
+        foreach (char c in word)
+        {
+            if (c != lastChar)
+            {
+                sb.Append(c);
+                lastChar = c;
+            }
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Lab7.Variant06.Tests/RepeatedLetterRemoverTests.cs
@@ -0,0 +1,51 @@
+﻿using Lab7.Variant06.Library;
+using Xunit;
+
+namespace Lab7.Variant06.Tests;
+
+public class RepeatedLetterRemoverTests
+{
+    private readonly RepeatedLetterRemover _repeatedLetterRemover = new RepeatedLetterRemover();
+
+    [Fact]
+    public void RemoveRepeatingCharacters_StandardCase_RemovesDuplicates()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("Hello world");
+        Assert.Equal("Helo world", result);
+    }
+
+    [Fact]
+    public void RemoveRepeatingCharacters_MixedCase_RemainsCaseSensitive()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("Coffeeee");
+        Assert.Equal("Cofe", result);
+    }
+
+    [Fact]
+    public void RemoveRepeatingCharacters_WithNumbersAndSymbols_RemovesDuplicates()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("112233!! hello!!");
+        Assert.Equal("123! helo!", result);
+    }
+
+    [Fact]
+    public void RemoveRepeatingCharacters_EmptyString_ReturnsEmpty()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void RemoveRepeatingCharacters_NoDuplicates_NoChange()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("Unique");
+        Assert.Equal("Unique", result);
+    }
+
+    [Fact]
+    public void RemoveRepeatingCharacters_SingleCharacterStrings_NoChange()
+    {
+        string result = _repeatedLetterRemover.RemoveRepeatingCharacters("a b c d e");
+        Assert.Equal("a b c d e", result);
+    }
+}
