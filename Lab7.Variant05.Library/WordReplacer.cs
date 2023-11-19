03ca549f58b1c2207ffa1819d5df24d000baa39e
Vladislav Sokolov <vsoilol1701@gmail.com>
2023-11-19 14:13:37 +0300

    add lab7 variant 5

---
+++ Lab7.Variant05.Library/WordReplacer.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Lab7.Variant05.Library;
+
+public class WordReplacer
+{
+    public string ReplaceWordInText(string text, string oldWord, string newWord)
+    {
+        // /b - Например, если oldWord - "cat", \bcat\b будет соответствовать "cat" в "a cat sat", но не в "category".
+        // Regex.Escape(oldWord) - Это необходимо, потому что, если old world содержит
+        // специальные символы (например, . или *), они должны рассматриваться как
+        // буквальные символы в контексте этого регулярного выражения.
+        // Например, если oldWord равно "c.at", без экранирования, оно
+        // будет соответствовать любому символу между c и t
+        // (из-за . и * являются метасимволами регулярного выражения), но при
+        // экранировании это соответствовало бы только буквальной строке "c.at".
+        string pattern = $@"\b{Regex.Escape(oldWord)}\b";
+        
+        // Заменить все соответсвия с pattern на newWord в text 
+        return Regex.Replace(text, pattern, newWord);
+    }
+}
---
+++ Lab7.Variant05.Tests/WordReplacerTests.cs
@@ -0,0 +1,58 @@
+﻿using Lab7.Variant05.Library;
+using Xunit;
+
+namespace Lab7.Variant05.Tests;
+
+public class WordReplacerTests
+{
+    private readonly WordReplacer _wordReplacer = new WordReplacer();
+
+    [Fact]
+    public void ReplaceWordInText_StandardCase_ReplacesCorrectly()
+    {
+        string result = _wordReplacer.ReplaceWordInText("The cat sat on the mat", "cat", "dog");
+        Assert.Equal("The dog sat on the mat", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_WithPunctuation_ReplacesCorrectly()
+    {
+        string result = _wordReplacer.ReplaceWordInText("Hello, world!", "world", "earth");
+        Assert.Equal("Hello, earth!", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_RegexSpecialCharacters_HandlesCorrectly()
+    {
+        string result = _wordReplacer.ReplaceWordInText("Find c.at here, not scat or concat.", "c.at", "cat");
+        Assert.Equal("Find cat here, not scat or concat.", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_WordNotPresent_NoChange()
+    {
+        string result = _wordReplacer.ReplaceWordInText("A quick brown fox jumps", "dog", "wolf");
+        Assert.Equal("A quick brown fox jumps", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_CaseSensitive_ReplacesOnlyMatchingCase()
+    {
+        string result = _wordReplacer.ReplaceWordInText("Cat and cat are different", "Cat", "Dog");
+        Assert.Equal("Dog and cat are different", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_EmptyString_ReturnsEmpty()
+    {
+        string result = _wordReplacer.ReplaceWordInText("", "hello", "hi");
+        Assert.Equal("", result);
+    }
+
+    [Fact]
+    public void ReplaceWordInText_MultipleOccurrences_ReplacesAll()
+    {
+        string result = _wordReplacer.ReplaceWordInText("cat cat cat", "cat", "dog");
+        Assert.Equal("dog dog dog", result);
+    }
+}
